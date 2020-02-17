fe35aba8b31c64949e91df7742ab788913e980ce
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-17 17:01:48 -0600

    delete works returns the whole list of accounts after deletion

--- server.js
+++ server.js
@@ -107,6 +107,28 @@ server.put('/api/accounts/:id', (req, res) =>
         res.status(500).json({error: 'Could not retrieve Accounts from the database'});
     })
 })
+
+server.delete('/api/accounts/:id', (req, res) =>
+{
+    db('accounts').where({id: req.params.id}).del()
+    .then(deletedAccount =>
+    {
+        db('accounts')
+        .then(accounts =>
+        {
+            res.status(200).json(accounts);
+        })
+        .catch(error =>
+        {
+            res.status(500).json({error: 'Could not retrieve Accounts from the database'});
+        })
+    })
+    .catch(error =>
+    {
+        res.status.json({error: 'Could not delete Account from the database'});
+    })
+})
+
 server.use('/', (req, res) =>
 {
     res.json({message: 'API is running'});
