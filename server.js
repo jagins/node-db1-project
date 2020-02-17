ed8498d2ad125aa186ede902ec76f6d834bbe22b
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-17 15:57:42 -0600

    get by id works

--- server.js
+++ server.js
@@ -19,6 +19,26 @@ server.get('/api/accounts', (req, res) =>
     })
 })
 
+server.get('/api/accounts/:id', (req, res) =>
+{
+    db('accounts').where({id: req.params.id}).first()
+    .then(account =>
+    {
+        if(account)
+        {
+            res.status(200).json(account);
+        }
+        else
+        {
+            res.status(404).json({message: 'Account with that ID does not exsist'});
+        }
+    })
+    .catch(error =>
+    {
+        res.status(500).json({error: 'Could not retrieve Accounts from the database'});
+    })
+})
+
 server.use('/', (req, res) =>
 {
     res.json({message: 'API is running'});
