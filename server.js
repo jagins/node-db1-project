921f39069dc2fcd9486e4b2423468016b79a2fa1
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-17 15:52:11 -0600

    get all accounts works

--- server.js
+++ server.js
@@ -6,4 +6,22 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/api/accounts', (req, res) =>
+{
+    db('accounts')
+    .then(accounts =>
+    {
+        res.status(200).json(accounts);
+    })
+    .catch(error =>
+    {
+        res.status(500).json({error: 'Could not retrieve Accounts from the database'});
+    })
+})
+
+server.use('/', (req, res) =>
+{
+    res.json({message: 'API is running'});
+})
+
 module.exports = server;
